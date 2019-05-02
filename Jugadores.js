49bebd0e0c5cfba3af8d748dc710d6c56b8e79a3
gracielajazmin <gracielajazmin_chavez@ucol.mx>
2019-05-02 13:50:57 -0500

    UPDATED

--- Jugadores.js
+++ Jugadores.js
@@ -11,11 +11,15 @@ class Jugador {
     set_posicion(nuevaPosicion){
         this.posicion += nuevaPosicion
         document.getElementById("Numero").innerHTML = "Posicion actual " + this.posicion;
+            if (this.posicion = movimiento()) {
+            return this.posicion
+        }
     }
 
     set_posicion2(nuevaPosicion){
         this.posicion += nuevaPosicion
         document.getElementById("Numero2").innerHTML = "Posicion actual " + this.posicion;
+
     }
 
     movimiento() {
@@ -23,36 +27,34 @@ class Jugador {
             return this.posicion + 58
         }
 
-        if (this.posicion == 14) {
+        else if (this.posicion == 14) {
             return this.posicion + 49
         }
 
-        if (this.posicion == 53) {
+        else if (this.posicion == 53) {
             return this.posicion + 19
         }
-        if (this.posicion == 64) {
+        else if (this.posicion == 64) {
             return this.posicion + 21
         }
 
-        if (this.posicion == 38) {
+        else if (this.posicion == 38) {
             return this.posicion - 18
         }
 
-        if (this.posicion == 51) {
+        else if (this.posicion == 51) {
             return this.posicion - 41
         }
 
-        if (this.posicion == 76) {
+        else if (this.posicion == 76) {
             return this.posicion - 22
         }
 
-        if (this.posicion == 91) {
+        else if (this.posicion == 91) {
             return this.posicion - 18
         }
 
-
-        if (this.posicion = 100) {
-            this.posicion = 100;
+        else if (this.posicion = 100) {
             console.log("Ganador");
         }
 
--- Main.js
+++ Main.js
@@ -28,6 +28,7 @@ var dado = new Dado();
 var random = dado.lanzar()
 jugador1.set_posicion(random);
 console.log(random)
+document.getElementById('dado').innerHTML = random
 }
 
 document.getElementById('lanzar2').addEventListener('click',lanzardado2)
@@ -36,6 +37,7 @@ var dado2 = new Dado();
 var random = dado2.lanzar()
 jugador2.set_posicion2(random);
 console.log(random)
+document.getElementById('dado').innerHTML = random
 }
 
 
@@ -43,26 +45,4 @@ console.log(random)
 
 
 
-/*
 
-function agregarjugador(){
-let player1 = new Jugador(document.getElementById('jugador1').value);
-let player2 = new Jugador(document.getElementById('jugador2').value);
-dado.lanzar(player1)
-dado.lanzar(player2)
-}
-
-
-function jugador1()
-{
-	document.getElementById('Numero').textContent = player1.jugar(dado.lanzar());
-}
-
-function jugador2()
-{
-	document.getElementById('Numero').textContent = player2.jugar(dado.lanzar());
-}
-
-document.getElementById('jugador1').addEventListener('click',jugador1);
-document.getElementById('jugador2').addEventListener('click',jugador2);
-*/
