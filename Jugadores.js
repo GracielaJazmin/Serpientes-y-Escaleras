5d8cf5d03b25defc64d3cfab6ed4daba7303b38d
gracielajazmin <gracielajazmin_chavez@ucol.mx>
2019-05-02 17:46:42 -0500

    Update Jugadores.js

--- Jugadores.js
+++ Jugadores.js
@@ -11,47 +11,49 @@ class Jugador {
     set_posicion(nuevaPosicion){
         this.posicion += nuevaPosicion
         document.getElementById("Numero").innerHTML = "Posicion actual " + this.posicion;
-            if (this.posicion = movimiento()) {
-            return this.posicion
-        }
+        this.movimiento(this.posicion);
+        return this.posicion
     }
 
     set_posicion2(nuevaPosicion){
         this.posicion += nuevaPosicion
         document.getElementById("Numero2").innerHTML = "Posicion actual " + this.posicion;
-
+        this.movimiento();
+        return this.posicion
     }
 
-    movimiento() {
-        if (this.posicion == 5) {
-            return this.posicion + 58
+    
+
+    movimiento(suma) {
+        if (this.posicion = 5) {
+             suma = this.posicion + 58
         }
 
-        else if (this.posicion == 14) {
-            return this.posicion + 49
+        else if (this.posicion = 14) {
+            suma = this.posicion + 49
         }
 
-        else if (this.posicion == 53) {
-            return this.posicion + 19
+        else if (this.posicion = 53) {
+            suma =  this.posicion + 19
         }
-        else if (this.posicion == 64) {
-            return this.posicion + 21
-        }
-
-        else if (this.posicion == 38) {
-            return this.posicion - 18
+        else if (this.posicion = 64) {
+            suma =  this.posicion + 21
         }
 
-        else if (this.posicion == 51) {
-            return this.posicion - 41
+        else if (this.posicion = 38) {
+            suma =  this.posicion - 18
         }
 
-        else if (this.posicion == 76) {
-            return this.posicion - 22
+        else if (this.posicion = 51) {
+            suma =  this.posicion - 41
         }
 
-        else if (this.posicion == 91) {
-            return this.posicion - 18
+        else if (this.posicion = 76) {
+            suma =  this.posicion - 22
+        }
+
+        else if (this.posicion = 91) {
+            suma =  this.posicion - 18
         }
 
         else if (this.posicion = 100) {
