efc940528f0a742130735ceacb5f888993f15d44
gracielajazmin <gracielajazmin_chavez@ucol.mx>
2019-05-02 21:10:24 -0500

    Update Jugadores.js

--- Jugadores.js
+++ Jugadores.js
@@ -11,53 +11,47 @@ class Jugador {
     set_posicion(nuevaPosicion){
         this.posicion += nuevaPosicion
         document.getElementById("Numero").innerHTML = "Posicion actual " + this.posicion;
-        if (this.posicion == 5) {
-        return this.posicion + 58
-        }
+  
     }
 
     set_posicion2(nuevaPosicion){
         this.posicion += nuevaPosicion
         document.getElementById("Numero2").innerHTML = "Posicion actual " + this.posicion;
-        if (this.posicion == 14) {
-           return this.posicion + 49
-         }
- 
     }
 
-    movimiento(posicion) {
-        if (posicion == 5) {
-             return posicion + 58
+    movimiento() {
+        if (this.posicion == 5) {
+             this.posicion += 58
         }
 
-        else if (posicion == 14) {
-            return posicion + 49
+         if (this.posicion == 14) {
+            this.osicion += 49
         }
 
-        else if (posicion == 53) {
-            return  posicion + 19
+         if (this.posicion == 53) {
+            this.posicion += 19
         }
-        else if (posicion == 64) {
-            return  posicion + 21
-        }
-
-        else if (posicion == 38) {
-            return  posicion - 18
+         if (this.posicion == 64) {
+            this.posicion += 21
         }
 
-        else if (posicion == 51) {
-            return  posicion - 41
+         if (this.posicion == 38) {
+            this.posicion -= 18
         }
 
-        else if (posicion == 76) {
-            return  posicion - 22
+         if (this.posicion == 51) {
+            this.posicion -= 41
         }
 
-        else if (posicion == 91) {
-            return  posicion - 18
+         if (this.posicion == 76) {
+            this.posicion -= 22
         }
 
-        else if (posicion == 100) {
+         if (this.posicion == 91) {
+            this.posicion -= 18
+        }
+
+         if (this.posicion == 100) {
             console.log("Ganador");
         }
 
